38d6b1bb6f05ce55265ebf7064b64a4f0c3cf2ca
veeck <michael.veeck@nebenan.de>
2023-02-10 10:14:11 +0100

    Add example on how font assets are used

--- sd.config.js
+++ sd.config.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
+const StyleDictionary = require('style-dictionary');
 
 const webPath = 'lib/';
 
@@ -6,6 +7,64 @@ const webPath = 'lib/';
 // to make sure they are ✨clean✨
 console.log(`cleaning ${webPath}...`);
 fs.removeSync(webPath);
+
+// Register an "attribute" transform to codify the font's details
+// as named attributes.
+StyleDictionary.registerTransform({
+  name: 'attribute/font',
+  type: 'attribute',
+  transformer: (prop) => ({
+    category: prop.path[0],
+    type: prop.path[1],
+    family: prop.path[2],
+    weight: prop.path[3],
+    style: prop.path[4],
+  }),
+});
+
+// Register a custom format to generate @font-face rules.
+StyleDictionary.registerFormat({
+  name: 'font-face',
+  formatter: ({ dictionary: { allTokens }, options }) => {
+    const fontPathPrefix = options.fontPathPrefix || '../';
+
+    // https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face/src
+    const formatsMap = {
+      woff2: 'woff2',
+      woff: 'woff',
+      ttf: 'truetype',
+      otf: 'opentype',
+      svg: 'svg',
+      eot: 'embedded-opentype',
+    };
+
+    return allTokens.reduce((fontList, prop) => {
+      const {
+        attributes: { family, weight, style },
+        formats,
+        value: path,
+      } = prop;
+
+      const urls = formats
+          .map((extension) => `url("${fontPathPrefix}${path}.${extension}") format("${formatsMap[extension]}")`);
+
+      const fontCss = [
+        '@font-face {',
+        `\n\tfont-family: "${family}";`,
+        `\n\tfont-style: ${style};`,
+        `\n\tfont-weight: ${weight};`,
+        `\n\tsrc: ${urls.join(',\n\t\t\t ')};`,
+        '\n\tfont-display: fallback;',
+        '\n}\n',
+      ].join('');
+
+      fontList.push(fontCss);
+
+      return fontList;
+    }, []).join('\n');
+  },
+});
+
 
 module.exports = {
   source: ['tokens/**/global_transformed.json'],
@@ -17,6 +76,7 @@ module.exports = {
         destination: '_index.css',
         format: 'css/variables',
       }],
+      actions: ['copy_assets'],
     },
     js: {
       transformGroup: 'js',
@@ -26,5 +86,24 @@ module.exports = {
         format: 'javascript/es6',
       }],
     },
+    'css-font-face': {
+      transforms: ['attribute/font'],
+      buildPath: webPath,
+      files: [
+        {
+          destination: 'fonts.css',
+          format: 'font-face',
+          filter: {
+            attributes: {
+              category: 'asset',
+              type: 'font',
+            },
+          },
+          options: {
+            fontPathPrefix: '../',
+          },
+        },
+      ],
+    }
   },
 };
