90d5f84ed393cbd70e32a899e4955cd03e7cc4ef
iamuchejude <iamuchejude@gmail.com>
2023-05-25 17:50:50 +0200

    Refactor codebase

--- build.js
+++ build.js
@@ -1,12 +1,14 @@
 const fs = require('fs-extra');
 const { registerTransforms } = require('@tokens-studio/sd-transforms');
 const StyleDictionary = require('style-dictionary');
+const iosColorsets = require('./actions/colorsets.ios');
+
+const BASE_BUILD_PATH = 'lib/';
+const WEB_PATH = `${BASE_BUILD_PATH}web/`;
+const IOS_PATH = `${BASE_BUILD_PATH}ios/`;
+const ANDROID_PATH = `${BASE_BUILD_PATH}android/`;
 
 registerTransforms(StyleDictionary);
-
-const webPath = 'lib/';
-const iosPath = 'styles/';
-const androidPath = 'android/'
 
 // Following my question: https://github.com/tokens-studio/sd-transforms/issues/31#issuecomment-1471665174
 // this was the solution for now to get cross-reference working.
@@ -32,42 +34,40 @@ StyleDictionary.registerParser({
 
 module.exports = {
   source: ['tokens/**/global.json'],
-  action: {
-    iosColorsets: require("./src/iOS/colorsets-action"),
-  },
+  action: { iosColorsets },
   platforms: {
     css: {
       transformGroup: 'tokens-studio',
-      buildPath: webPath,
+      buildPath: WEB_PATH,
       files: [{
-        destination: '_index.css',
+        destination: 'index.css',
         format: 'css/variables',
       }],
     },
     js: {
       transformGroup: 'tokens-studio',
-      buildPath: webPath,
+      buildPath: WEB_PATH,
       files: [{
         destination: 'index.js',
         format: 'javascript/es6',
       }],
     },
-    iosColorsets: {
-      buildPath: iosPath,
+    ios: {
+      buildPath: IOS_PATH,
       transforms: ['attribute/cti', 'name/cti/pascal', 'attribute/color'],
       actions: ['iosColorsets'],
     },
     android: {
-          transformGroup: 'android',
-          buildPath: androidPath,
-          files: [{
-            destination: 'design_token_colors.xml',
-            format: 'android/colors'
-          }]
-        },
+      transformGroup: 'android',
+      buildPath: ANDROID_PATH,
+      files: [{
+        destination: 'design_token_colors.xml',
+        format: 'android/colors'
+      }]
+    },
     compose: {
       transformGroup: 'compose',
-      buildPath: androidPath,
+      buildPath: ANDROID_PATH,
       files: [{
         destination: 'DesignTokenColor.kt',
         format: 'compose/object',
