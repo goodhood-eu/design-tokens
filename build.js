35e9a60395a09cb8433288e3782b2f65459b6c5e
veeck <michael.veeck@nebenan.de>
2023-11-23 11:34:54 +0100

    Add example on how font assets are used

--- build.js
+++ build.js
@@ -38,7 +38,7 @@ StyleDictionary.registerTransform(removePrefixForAndroid)
 StyleDictionary.registerTransform(removePrefixForCompose)
 StyleDictionary.registerTransform(addMissingUnits)
 module.exports = {
-    source: ['tokens/**/global.json'],
+    source: ['tokens/**/global.json', 'tokens/asset/fonts.json'],
     action: {iosColorsets},
     platforms: {
         css: {
@@ -48,6 +48,7 @@ module.exports = {
                 destination: 'index.css',
                 format: 'css/variables',
             }],
+            actions: ['copy_assets'],
         }, scss: {
             transforms: [
                 'ts/descriptionToComment',
@@ -77,6 +78,25 @@ module.exports = {
                 destination: 'index.js',
                 format: 'javascript/es6',
             }],
+        },
+        'css-font-face': {
+            transforms: ['attribute/font'],
+            buildPath: WEB_PATH,
+            files: [
+                {
+                    destination: 'fonts.css',
+                    format: 'font-face',
+                    filter: {
+                        attributes: {
+                            category: 'asset',
+                            type: 'font',
+                        },
+                    },
+                    options: {
+                        fontPathPrefix: '../',
+                    },
+                },
+            ],
         },
         ios: {
             buildPath: IOS_PATH,
@@ -110,3 +130,60 @@ module.exports = {
         },
     },
 };
+
+// Register an "attribute" transform to codify the font's details
+// as named attributes.
+StyleDictionary.registerTransform({
+    name: 'attribute/font',
+    type: 'attribute',
+    transformer: (prop) => ({
+        category: prop.path[0],
+        type: prop.path[1],
+        family: prop.path[2],
+        weight: prop.path[3],
+        style: prop.path[4],
+    }),
+});
+
+// Register a custom format to generate @font-face rules.
+StyleDictionary.registerFormat({
+    name: 'font-face',
+    formatter: ({ dictionary: { allTokens }, options }) => {
+        const fontPathPrefix = options.fontPathPrefix || '../';
+
+        // https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face/src
+        const formatsMap = {
+            woff2: 'woff2',
+            woff: 'woff',
+            ttf: 'truetype',
+            otf: 'opentype',
+            svg: 'svg',
+            eot: 'embedded-opentype',
+        };
+
+        return allTokens.reduce((fontList, prop) => {
+            const {
+                attributes: { family, weight, style },
+                formats,
+                value: path,
+            } = prop;
+
+            const urls = formats
+                .map((extension) => `url("${fontPathPrefix}${path}.${extension}") format("${formatsMap[extension]}")`);
+
+            const fontCss = [
+                '@font-face {',
+                `\n\tfont-family: "${family}";`,
+                `\n\tfont-style: ${style};`,
+                `\n\tfont-weight: ${weight};`,
+                `\n\tsrc: ${urls.join(',\n\t\t\t ')};`,
+                '\n\tfont-display: fallback;',
+                '\n}\n',
+            ].join('');
+
+            fontList.push(fontCss);
+
+            return fontList;
+        }, []).join('\n');
+    },
+});
